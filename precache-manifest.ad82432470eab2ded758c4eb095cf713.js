10b5e2a9ec4243d4c96b7ddc35eea2a2a96b6461
Brian Foster <brriann@gmail.com>
2020-02-23 13:58:59 -0700

    Updates

---
+++ precache-manifest.ad82432470eab2ded758c4eb095cf713.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e17f3b97d7075f200bd4a197529437b",
+    "url": "/brriann.github.io/index.html"
+  },
+  {
+    "revision": "b7bb305dc9065dcf9540",
+    "url": "/brriann.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4b1760d2ba87fdea655f",
+    "url": "/brriann.github.io/static/js/2.27bd20b4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/brriann.github.io/static/js/2.27bd20b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7bb305dc9065dcf9540",
+    "url": "/brriann.github.io/static/js/main.5d9021bd.chunk.js"
+  },
+  {
+    "revision": "cc6ca03cd4ab9263c05c",
+    "url": "/brriann.github.io/static/js/runtime-main.4c6d8f7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brriann.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
