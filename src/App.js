dcf2684ef2ff0224d9e95ad1e9ab18cb41a6c3ec
Brian Foster <brriann@gmail.com>
2020-02-23 14:46:28 -0700

    clean up project update README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to brriann.github.io.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          This is a collection of projects with some technical discussion included.
+        </p>
+        <p>
+          Many are in flux, so please check back often.
+        </p>
+        <p>Visit in a browser:</p>
+        <p className="App-link-list">
+          <span>
+            <a
+            className="App-link"
+            href="https://fantasy.ski"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              fantasy.ski
+            </a>
+          </span>
+          <span>
+            <a
+            className="App-link"
+            href="http://keydev.us"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              keydev.us
+            </a>
+          </span>
+          <span>
+            <a
+            className="App-link"
+            href="http://floatapp.net"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              floatapp.net
+            </a>
+          </span>
+          <span>
+            <a
+            className="App-link"
+            href="http://bfostdev.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              bfostdev.com
+            </a>
+          </span>
+          <span>
+            <a
+            className="App-link"
+            href="http://bfostdev.net"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              bfostdev.net
+            </a>
+          </span>
+        </p>
+        <p>
+          Or view in a repository:
+        </p>
+        <p className="App-link-list">
+        <span>
+            <a
+            className="App-link"
+            href="https://github.com/brriann/brriann.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              brriann.github.io
+            </a>
+          </span>
+          <span>
+            <a
+            className="App-link"
+            href="https://github.com/brriann/brriann.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              algs-ds
+            </a>
+          </span>
+        </p>
       </header>
     </div>
   );
